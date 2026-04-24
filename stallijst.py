424ee1de53d4fe16d372215c59a4a5b61d59bb3c
jce <33622307+jce@users.noreply.github.com>
2026-04-24 17:06:15 +0200

    clarity

--- stallijst.py
+++ stallijst.py
@@ -1,5 +1,5 @@
 #!/bin/python3
-import json
+import json, datetime
 fokker =    "BREEDER_NAME_HERE"
 
 def main():
@@ -11,29 +11,16 @@ def main():
     print("Fokkerij nummer: {}".format(db["fokker"][fokker]["VNLid"]))
     print("UBN: {}".format(db["fokker"][fokker]["UBN"]))
     print("Adres: {} {}".format(db["fokker"][fokker]["adres"], db["fokker"][fokker]["woonplaats"]))
+    print("Datum: {}".format(datetime.datetime.now().strftime("%d/%m/%Y")))
     print("")
-    print("                     geslacht status                              lammen")
+    print("                     geslacht status                            lammeren")
     print("nr stamboeknr  levensnr     | | geboren    vader       moeder   worpen |")
-    i = 0
-    for s in db["fokker"][fokker]["stal"]:
-        i += 1
-        VNLid = db["schaap"][s]["VNLid"]
-        LevensNr = db["schaap"][s]["LevensNr"]
-        Geslacht = db["schaap"][s]["Geslacht"]
-        Status = db["schaap"][s]["Status"]
-        Gebdat = db["schaap"][s]["Gebdat"]
-        Vader = db["schaap"][s]["Vader"]
-        Moeder = db["schaap"][s]["Moeder"]
-        print("{:2} {:11} {:12} {:1} {:1} {:10} {:11} {:11}".format(i, VNLid, LevensNr, Geslacht, Status, Gebdat, Vader, Moeder), end="")
-        try:
-            print(" {:2}".format(db["schaap"][s]["Aantal worpen"]), end="")
-        except KeyError:
-            print("   ", end="")
-        try:
-            print(" {:2}".format(db["schaap"][s]["Aantal lammeren"]), end="")
-        except KeyError:
-            print("    ", end="")
-        print("")
+    for i, s_id in enumerate(db["fokker"][fokker]["stal"], start=1):
+        s = db["schaap"][s_id]
+        worpen = s.get("Aantal worpen", "")
+        lammeren = s.get("Aantal lammeren", "")
+        print(  f"{i:2} {s['VNLid']:11} {s['LevensNr']:12} {s['Geslacht']:1} {s['Status']:1} "
+                f"{s['Gebdat']:10} {s['Vader']:11} {s['Moeder']:11} {worpen:2} {lammeren:2}")
 
 if __name__ == "__main__":
 	main()
